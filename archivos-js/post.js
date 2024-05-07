45b9fad13b8261a4fe6051a65dd69d805bfbbe4e
maribelcongo <congoceleste1@gmail.com>
2024-05-07 19:52:21 -0300

    formulario para crear nuevo coctel

---
+++ archivos-js/post.js
@@ -0,0 +1,67 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+            document.getElementById('toggleFormularioBtn').addEventListener('click', function() {
+              
+                var elementsToHide = document.querySelectorAll('main > *:not(#nuevaCoctelCard)');
+                elementsToHide.forEach(function(element) {
+                    element.style.display = 'none';
+                });
+
+                // Mostrar el formulario
+                document.getElementById('nuevaCoctelCard').style.display = 'block';
+            });
+
+            document.getElementById('cancelarBtn').addEventListener('click', function() {
+            
+                let elementsToShow = document.querySelectorAll('main > *:not(#nuevaCoctelCard)');
+                elementsToShow.forEach(function(element) {
+                    element.style.display = 'block';
+                });
+
+                // Ocultar el formulario
+                document.getElementById('nuevaCoctelCard').style.display = 'none';
+            });
+
+            document.getElementById('coctelForm').addEventListener('submit', function(event) {
+                event.preventDefault(); 
+
+                let nombre = document.getElementById('nameInput').value;
+                let preparacion = document.getElementById('preparationInput').value;
+                let imagen = document.getElementById('imageInput').value;
+                let alcohol = document.getElementById('alcoholInput').value;
+                let ingredientes = document.getElementById('ingredientsInput').value.split(',');
+
+                let nuevoCoctel = {
+                    nombre: nombre,
+                    preparacion: preparacion,
+                    imagen: imagen,
+                    alcohol: alcohol,
+                    ingredientes: ingredientes
+                };
+
+                fetch(baseUrl, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(nuevoCoctel)
+                })
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Error al crear el coctel');
+                    }
+                    return response.json();
+                })
+                .then(data => {
+                    console.log('Coctel creado exitosamente:', data);
+                   
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                  
+                });
+            });
+        });
+
+
+        
